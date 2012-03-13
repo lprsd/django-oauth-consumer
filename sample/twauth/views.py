6aab05ad6bc19c89e939aba0a6ccbff2cabc7e7c
Lakshman Prasad <lakshman@lakshmanprasad.com>
2012-03-13 05:40:51 +0530

    A little move around.

--- sample/twauth/views.py
+++ sample/twauth/views.py
@@ -1,10 +1,16 @@
 # Create your views here.
+key = "dEzyfctNcw6D4oJ535gJBA"
+secret = "WIHKnttvcKTFYnZG1S3gaFN9sEmqMXtfVApCpQb9RJI"
+request_url = "https://api.twitter.com/oauth/request_token"
+auth_url = "http://api.twitter.com/oauth/authorize"
+access_url = "https://api.twitter.com/oauth/access_token"
+update_url = "http://api.twitter.com/1/statuses/update.json"
 
-def seek_permission():
+def seek_authorization():
     pass
 
 def tweet():
     pass
 
-def permission_obtained():
+def authorization_callback():
     pass
